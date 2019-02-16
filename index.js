4ab850ded31ac2fc084cc900c02372d7a606477c
Riku Sakamoto <rikusaka03@gmail.com>
2019-02-16 22:01:36 +0900

    index.html

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// 外部ファイル
+var TweetModule = require('./sample');
+
 // expressモジュールを読み込む
 const express = require('express');
 
@@ -6,7 +9,7 @@ const app = express();
 
 //Heroku へデプロイした際は、ポートは process.env.PORT が適用される
 app.set('port',(process.env.PORT||5000))
-//app.use(express.static('public'));
+app.use(express.static('public'));
 
 // ルート（http://localhost/）にアクセスしてきたときに「Hello」を返す
 app.get('/api', (req, res) => res.send(
@@ -18,3 +21,6 @@ app.listen(app.get('port'),function(){
   console.log("Node app is running at localhost:" + app.get('port'));
 })
 
+function Tweet(){
+  TweetModule.FunTweeet("Hello Diet World !")
+}
